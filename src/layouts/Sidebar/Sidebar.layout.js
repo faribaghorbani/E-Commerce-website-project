2acd9ed9a4b9a85bd415bd6330106d982bab12d2
Fariba <fariba.ghorbani2127@gmail.com>
2022-05-05 07:04:37 +0430

    Constructing all the components/add Loading
    - add all the components of any page
    - add general components
    - add loading page

---
+++ src/layouts/Sidebar/Sidebar.layout.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SidebarLayout = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SidebarLayout;
---
+++ src/pages/Notfound/Components/Warning.component.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Warning = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Warning;
