c5a2e02aae39fd231c6874e6231ce08f46efc486
Fariba <fariba.ghorbani2127@gmail.com>
2022-05-10 22:31:19 +0430

    fix: Delete extra import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.module.css'
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './routes/App.route'
 import { Provider } from 'react-redux';
