cff0a98846d3c69e06dfe142620a09908f4c4715
Fariba <fariba.ghorbani2127@gmail.com>
2022-05-15 17:27:35 +0430

    feat: Add font to mui theme in index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
 import App from './routes/App.route'
+import './index.scss'
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store }  from './redux/store'
 import CssBaseline from '@mui/material/CssBaseline';
+import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
 
 // document.dir='rtl'
+const theme = createTheme({
+  typography: {
+    fontFamily: 'shabnam, Arial',
+  },
+});
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-  <Provider store={store}>
-    <Router>
-      <CssBaseline />
-      <App />
-    </Router>
-  </Provider>
+    <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <Router>
+            <CssBaseline />
+            <App />
+          </Router>
+        </ThemeProvider>
+    </Provider>
   // </React.StrictMode>
 );
