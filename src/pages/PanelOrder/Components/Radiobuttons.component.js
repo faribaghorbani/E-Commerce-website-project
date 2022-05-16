fb5cf246c014822b2ca914fd63df0df8b08973d0
Fariba <fariba.ghorbani2127@gmail.com>
2022-05-16 18:18:17 +0430

    style: Rename Radiobuttons component
    - Radiobuttons used in filtering of orders in admin panel

---
+++ src/pages/PanelOrder/Components/Radiobuttons.component.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+import { useDispatch } from 'react-redux';
+import { setOrdersStatus } from '../../../redux/slices/ordersStatusSlices'
+import RTL from '../../../components/RTL.component';
+
+export default function RadioButtonsGroup() {
+	  const [value, setValue] = useState('');
+  	const dispatch = useDispatch()
+
+	
+    const handleChange = (e) => {
+      setValue(e.target.value);
+      dispatch(setOrdersStatus(e.target.value))
+    }
+
+
+    return (
+      <FormControl>
+        <RTL>
+        <FormLabel id="demo-radio-buttons-group-label">فیلتر سفارش ها:</FormLabel>
+        <RadioGroup
+          aria-labelledby="demo-radio-buttons-group-label"
+          name="radio-buttons-group"
+          value={value}
+          onChange={handleChange}
+        >
+          <FormControlLabel value="0" control={<Radio />} label="همه سفارش ها" />
+          <FormControlLabel value="1" control={<Radio />} label="سفارش های تحویل داده شده" />
+          <FormControlLabel value="2" control={<Radio />} label="سفارش های در حال ارسال " />
+          <FormControlLabel value="3" control={<Radio />} label="سفارش های لغو شده" />
+        </RadioGroup>
+        </RTL>
+      </FormControl>
+    );
+}
