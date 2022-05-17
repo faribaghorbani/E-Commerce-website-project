7c67e47c4c7eadd7323f161af121b9fc991e83bf
Fariba <fariba.ghorbani2127@gmail.com>
2022-05-17 23:26:08 +0430

    refactor: Move sidebar from layout to a component

--- src/layouts/Sidebar/Components/Sidebar.component.js
+++ src/layouts/Sidebar/Components/Sidebar.component.js
@@ -1,11 +1,51 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import Collapse from '@mui/material/Collapse';
+import ExpandLess from '@mui/icons-material/ExpandLess';
+import ExpandMore from '@mui/icons-material/ExpandMore';
+import { useSelector } from 'react-redux';
 
 const SidebarComponent = () => {
+    const categoryData = useSelector(state => state.categoryData)
+	const [openStatus, setOpenStatus] = useState({});
+
+    
+	useEffect(() => {
+		categoryData.map((category) => {
+			setOpenStatus(prevState => ({...prevState, [category.name]: false}))
+		})
+	}, [categoryData])
+
 
     return (
-        <div>
-        
-        </div>
+        <List>
+            {categoryData.map((item) => {
+                return (
+                    <React.Fragment key={item.id}>
+                        <ListItemButton onClick={() => {
+                            const temp = openStatus[item.name]
+                            setOpenStatus(prevState => ({...prevState, [item.name]: !temp}))
+                        }}>
+                            <ListItemText primary={item.title} />
+                            {openStatus[item.name] ? <ExpandLess /> : <ExpandMore />}
+                        </ListItemButton>
+                        <Collapse in={openStatus[item.name]} timeout="auto" unmountOnExit>
+                            <List component="div" disablePadding>
+                                {item.subCategory.map((sub) => {
+                                    return (
+                                    <ListItemButton key={sub.id} sx={{ pl: 4 }}>
+                                        <ListItemText primary={sub.title} />
+                                    </ListItemButton>
+                                    )
+                                })}
+                            </List>
+                        </Collapse>
+                    </React.Fragment>
+                )
+            })}
+        </List>
     )
 }
 
--- src/layouts/Sidebar/Sidebar.layout.js
+++ src/layouts/Sidebar/Sidebar.layout.js
@@ -1,36 +1,18 @@
 import { Grid } from '@mui/material';
-import { get } from 'lodash';
 import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import LoadingPage from '../../pages/Loading/Loading.page';
 import { setCategoryData } from '../../redux/slices/categoryDataSlice';
 import { getDataUser } from '../../services/http.service';
 import Header from './Components/Header.component';
-import List from '@mui/material/List';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import Collapse from '@mui/material/Collapse';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import DraftsIcon from '@mui/icons-material/Drafts';
-import SendIcon from '@mui/icons-material/Send';
-import ExpandLess from '@mui/icons-material/ExpandLess';
-import ExpandMore from '@mui/icons-material/ExpandMore';
-import StarBorder from '@mui/icons-material/StarBorder';
-
-
+import SidebarComponent from './Components/Sidebar.component';
 
 
 const SidebarLayout = (props) => {
     const [loading, setLoading] = useState(true)
     const [error, setError] = useState(false)
-	const categoryData = useSelector(state => state.categoryData)
 	const dispatch = useDispatch()
-	const [openStatus, setOpenStatus] = useState({});
 
-	// const handleClick = () => {
-	// 	setOpen(!open);
-	// };
 
     useEffect(() => {
 		getDataUser('/category?_embed=subCategory', 
@@ -44,16 +26,6 @@ const SidebarLayout = (props) => {
 		})
     }, [])
 
-	useEffect(() => {
-		console.log(categoryData)
-		categoryData.map((category) => {
-			setOpenStatus(prevState => ({...prevState, [category.name]: false}))
-		})
-	}, [categoryData])
-
-	useEffect(() => {
-		console.log(openStatus)
-	}, [openStatus])
 
 	if (loading) {
 		return (
@@ -79,34 +51,7 @@ const SidebarLayout = (props) => {
 						{props.children}
 					</Grid>
 					<Grid item xs={3}>
-						<List>
-							{categoryData.map((item) => {
-								// console.log(item)
-								return (
-									<React.Fragment key={item.id}>
-									<ListItemButton onClick={() => {
-										const temp = openStatus[item.name]
-										setOpenStatus(prevState => ({...prevState, [item.name]: !temp}))
-									}}>
-										<ListItemText primary={item.title} />
-										{openStatus[item.name] ? <ExpandLess /> : <ExpandMore />}
-									</ListItemButton>
-									<Collapse in={openStatus[item.name]} timeout="auto" unmountOnExit>
-										<List component="div" disablePadding>
-											{item.subCategory.map((sub) => {
-												console.log(sub)
-												return (
-												<ListItemButton key={sub.id} sx={{ pl: 4 }}>
-													<ListItemText primary={sub.title} />
-												</ListItemButton>
-												)
-											})}
-										</List>
-									</Collapse>
-								</React.Fragment>
-								)
-							})}
-						</List>
+						<SidebarComponent />
 					</Grid>
 				</Grid>
 			</>
