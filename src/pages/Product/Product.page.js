77188f86545779ca27143abaec0236e20bc2593b
Fariba <fariba.ghorbani2127@gmail.com>
2022-05-18 16:20:29 +0430

    feat: Construct the basis of product page
    - single product pagge framework

--- src/pages/Product/Product.page.js
+++ src/pages/Product/Product.page.js
@@ -1,11 +1,62 @@
-import React from 'react'
+import { Paper } from '@mui/material';
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+import { getDataUser } from '../../services/http.service';
+import LoadingPage from '../Loading/Loading.page';
+import RTL from '../../components/RTL.component';
 
 const ProductPage = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+	const params = useParams()
+	const [data, setData] = useState([])
+	const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(false)
+
+	useEffect(() => {
+		getDataUser(`/products?id=${params.id}`,
+			(data) => {
+				setData(data)
+				setLoading(false)
+			},
+			() => {
+				setLoading(false)
+				setError(true)
+			}
+		)
+		console.log(data);
+	}, [])
+
+	useEffect(() => {
+		console.log(data)
+	}, [data])
+
+	if (loading) {
+		return (
+			<>
+				<LoadingPage/>
+			</>
+		)
+	} else if (error) {
+		return (
+			<>
+				<div>اتصال به سرور با خطا رو به رو شد</div>
+			</>
+		)
+	} else {
+		return (
+			<>
+			<Paper elevation={2}>
+
+				{data.map(item => {
+					return (
+						<div dir="rtl">
+							<React.Fragment key={item.id}>{item.name}</React.Fragment>
+						</div>
+					)
+				})}
+			</Paper>
+			</>
+		)
+	}
 }
 
 export default ProductPage;
