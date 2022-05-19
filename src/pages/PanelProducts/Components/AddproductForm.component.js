0dbc4845b7eb8145ad125ed465f72bcd0cfcaa78
Fariba <fariba.ghorbani2127@gmail.com>
2022-05-19 08:02:48 +0430

    feat: Cosntruct the fields and texteditor in form
    - add CKeditor
    - add conditional subCategory select menu

--- src/pages/PanelProducts/Components/AddproductForm.component.js
+++ src/pages/PanelProducts/Components/AddproductForm.component.js
@@ -6,12 +6,21 @@ import LoadingComponent from '../../Loading/Components/Loading.component'
 import { Form, useFormik } from 'formik';
 import axios from 'axios'
 import TextField from '@mui/material/TextField';
+import RTL from '../../../components/RTL.component'
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { Box } from '@mui/system'
 
 
 const AddproductForm = () => {
 	const categoryData = useSelector(state => state.categoryData)
 	const [loading, setLoading] = useState(true)
     const [error, setError] = useState(false)
+	const [group, setGroup] = useState('');
 	const [files, setFiles] = useState([])
 	const [thumbnail, setThumbnail] = useState()
 	const [uploads, setUploads] = useState([])
@@ -21,13 +30,18 @@ const AddproductForm = () => {
 		brand: '',
 		price: '',
 		quantity: '',
-		category: {},
+		category: {
+			main: '',
+			second: ''
+		},
 		description: '',
 		thumbnail: '',
 		gallery: [],
 		color: []
 	})
 	const dispatch = useDispatch()
+
+
 
 
 	useEffect(() => {
@@ -68,7 +82,7 @@ const AddproductForm = () => {
 	}
 
 	useEffect(() => {
-		console.log(uploads)
+		console.log(values)
 		// const formData = new FormData()
 		// formData.append('image', files[files.length-1])
 		// if (files.length === 0) {
@@ -76,21 +90,12 @@ const AddproductForm = () => {
 		// 		.then(res => res.data)
 		// 		.then(data => setUploads(prev => [...prev, data.filename]))
 		// }
-	}, [uploads])
+	}, [values])
 
 
 	if (loading) {
 		return (
 			<div>
-				<TextField
-					dir='rtl'
-					sx={{mb: 3}}
-					id="standard-password-input"
-					label="نام کالا"
-					type="text"
-					name="name"
-					variant="standard"
-				/>
 				<LoadingComponent />
 			</div>
 		
@@ -104,13 +109,130 @@ const AddproductForm = () => {
 	} else {
 		return (
 			<div>
-				{/* <input type="file" multiple onChange={e => setFiles(prev => [...prev, e.target.files[0]])}/> */}
-				<input type="file" multiple onChange={e => setThumbnail(e.target.files[0])}/>
-				<input type="file" multiple onChange={e => {
-					console.log(e.target.files)
-					setFiles(Object.values(e.target.files))
-				}}/>
-				<button onClick={submitTheForm}>Hello</button>
+				<RTL>
+					<TextField
+						fullWidth
+						dir='rtl'
+						sx={{mb: 3}}
+						id="standard-password-input1"
+						label="نام کالا"
+						type="text"
+						name="name"
+						variant="standard"
+						onChange={e => setValues(prev => ({...prev, 'name': e.target.value}))}
+					/>
+					<TextField
+						fullWidth
+						dir='rtl'
+						sx={{mb: 2}}
+						id="standard-password-input2"
+						label="قیمت"
+						type="number"
+						name="price"
+						variant="standard"
+						onChange={e => setValues(prev => ({...prev, 'price': e.target.value}))}
+					/>
+					<TextField
+						fullWidth
+						dir='rtl'
+						sx={{mb: 2}}
+						id="standard-password-input3"
+						label="برند"
+						type="text"
+						name="brand"
+						variant="standard"
+						onChange={e => setValues(prev => ({...prev, 'brand': e.target.value}))}
+					/>
+					<div>
+						<FormControl dir='rtl' fullWidth sx={{ m: 1, minWidth: 80 }}>
+							<InputLabel id="demo-simple-select-autowidth-label1">دسته بندی سرگروه</InputLabel>
+								<Select
+								labelId="demo-simple-select-autowidth-label1"
+								id="demo-simple-select-autowidth1"
+								value={values.category.main}
+								onChange={e => setValues(prev => ({...prev, 'category':{...values.category, 'main': e.target.value}}))}
+								autoWidth
+								label="دسته بندی سرگروه"
+								>
+								<MenuItem value="">
+									<em>-</em>
+								</MenuItem>
+								{categoryData.map(itemCat => {
+									return (
+										<MenuItem value={itemCat.name}>{itemCat.title}</MenuItem>
+									)
+								})}
+							</Select>
+						</FormControl>
+					</div>
+					<div>
+						<FormControl dir='rtl' fullWidth sx={{ m: 1, minWidth: 80 }} disabled={values.category.main == ""}>
+							<InputLabel id="demo-simple-select-autowidth-label2">دسته بندی زیرگروه</InputLabel>
+								<Select
+								labelId="demo-simple-select-autowidth-label2"
+								id="demo-simple-select-autowidth2"
+								value={values.category.second}
+								onChange={e => setValues(prev => ({...prev, 'category':{...values.category, 'second': e.target.value}}))}
+								autoWidth
+								label="دسته بندی زیرگروه"
+								>
+								<MenuItem value="">
+									<em>-</em>
+								</MenuItem>
+								{categoryData
+									?.filter(itemCat => itemCat.name == values.category.main)[0]?.subCategory
+									?.map((sub) => {
+										return (
+											<MenuItem value={sub.name}>{sub.title}</MenuItem>
+										)
+									})
+								}
+							</Select>
+						</FormControl>
+					</div>
+					<TextField
+						fullWidth
+						dir='rtl'
+						sx={{mb: 2}}
+						id="standard-password-input4"
+						label="موجودی"
+						type="number"
+						name="quantity"
+						variant="standard"
+						onChange={e => setValues(prev => ({...prev, 'quantity': e.target.value}))}
+					/>
+
+					<CKEditor
+						editor={ ClassicEditor }
+						data=""
+						onReady={ editor => {
+							// You can store the "editor" and use when it is needed.
+							console.log( 'Editor is ready to use!', editor );
+						} }
+						onChange={ ( event, editor ) => {
+							const data = editor.getData();
+							setValues(prev => ({...prev, 'description': data}))
+						} }
+						// onBlur={ ( event, editor ) => {
+						// 	console.log( 'Blur.', editor );
+						// } }
+						// onFocus={ ( event, editor ) => {
+						// 	console.log( 'Focus.', editor );
+						// } }
+					/>
+					<Box>
+						<label>آپلود تامبنیل</label>
+						<input type="file" multiple onChange={e => setThumbnail(e.target.files[0])}/>
+					</Box>
+					<Box>
+						<label>آپلود گالری</label>
+						<input type="file" multiple onChange={e => {
+							console.log(e.target.files)
+							setFiles(Object.values(e.target.files))
+						}}/>
+					</Box>
+					<button onClick={submitTheForm}>Hello</button>
+				</RTL>
 			</div>
 		)
 	}
