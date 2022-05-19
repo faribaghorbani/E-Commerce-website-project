8cdd039698fae4a01de174f351acf2b4a45287dd
Fariba <fariba.ghorbani2127@gmail.com>
2022-05-19 13:05:51 +0430

    feat: Add configuration for GallerySlider
    - add a component
    - add scss

--- src/pages/Product/Components/GallerySlider.component.js
+++ src/pages/Product/Components/GallerySlider.component.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { Component } from 'react';
+import ImageGallery from 'react-image-gallery';
 
-const GallerySlider = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+const images = [
+  {
+    original: 'https://picsum.photos/id/1018/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1018/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1015/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1015/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1019/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1019/250/150/',
+  },
+];
+
+
+class GallerySlider extends Component {
+	constructor(props) {
+		super(props)
+	}
+  	render() {
+    	return <ImageGallery items={images} />;
+  	}
 }
 
 export default GallerySlider;
