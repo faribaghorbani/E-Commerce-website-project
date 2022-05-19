0a3a675d348439ff88aede6f858ca6f006035202
Fariba <fariba.ghorbani2127@gmail.com>
2022-05-19 16:15:27 +0430

    feat: Add cards in home page

--- src/pages/Home/Components/Card.component.js
+++ src/pages/Home/Components/Card.component.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
 
-const CardComponent = () => {
+export default function CardComponent(props) {
   return (
-    <div>
-      
-    </div>
-  )
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+      <CardMedia
+          component="img"
+          height="140"
+          image={props.image}
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography dir='rtl' gutterBottom variant="h5" component="div">
+            {props.title}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
 }
-
-export default CardComponent;
--- src/pages/Home/Home.page.js
+++ src/pages/Home/Home.page.js
@@ -4,6 +4,9 @@ import laptop1 from '../../assets/images/gallery-1-2.jpg'
 import laptop2 from '../../assets/images/gallery-8-1.jpg'
 import laptop3 from '../../assets/images/laptop2.jpg'
 import laptop4 from '../../assets/images/laptop4.jpg'
+import './Home.module.scss'
+import { Box } from '@mui/system';
+import CardComponent from './Components/Card.component';
 
 const HomePage = () => {
 const images = [
@@ -28,7 +31,55 @@ const images = [
   return (
     <div>
       <GallerySlider images={images} />
-      
+      {/* <div className='body'>
+        <div className="container">
+          <div className="card">
+              <div className="face face1">
+                  <div className="content">
+                      <img src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/design_128.png?raw=true" />
+                      <h3>Design</h3>
+                  </div>
+              </div>
+              <div className="face face2">
+                  <div className="content">
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cum cumque minus iste veritatis provident at.</p>
+                          <a href="#">Read More</a>
+                  </div>
+              </div>
+          </div>
+          <div className="card">
+              <div className="face face1">
+                  <div className="content">
+                      <img src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/code_128.png?raw=true" />
+                      <h3>Code</h3>
+                  </div>
+              </div>
+              <div className="face face2">
+                  <div className="content">
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cum cumque minus iste veritatis provident at.</p>
+                          <a href="#">Read More</a>
+                  </div>
+              </div>
+          </div>
+          <div className="card">
+              <div className="face face1">
+                  <div className="content">
+                      <img src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/launch_128.png?raw=true" />
+                      <h3>Launch</h3>
+                  </div>
+              </div>
+              <div className="face face2">
+                  <div className="content">
+                      <p>Lorem ipsum dolor si t amet consectetur adipisicing elit. Quas cum cumque minus iste veritatis provident at.</p>
+                          <a href="#">Read More</a>
+                  </div>
+              </div>
+          </div>
+        </div>
+      </div> */}
+      {/* <Box>
+        <CardComponent image={} title={"آلترا بوک"} />
+      </Box> */}
     </div>
   )
 }
