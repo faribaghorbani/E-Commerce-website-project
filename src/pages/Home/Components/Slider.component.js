00239afe1ae78eb056aebedbb21e7e648a854b2c
Fariba <fariba.ghorbani2127@gmail.com>
2022-05-25 19:21:05 +0430

    feat:Complete a demo of infinite loop slider(home)
    - home page slider for cards of each category

--- src/pages/Home/Components/Slider.component.js
+++ src/pages/Home/Components/Slider.component.js
@@ -1,11 +1,40 @@
 import React from 'react'
-import './style/Slider.module.scss'
+import './style/Slider.scss'
+import laptop1 from './../../../assets/images/gallery-1-2.jpg'
+import laptop2 from './../../../assets/images/gallery-8-1.jpg'
+import laptop3 from './../../../assets/images/laptop2.jpg'
+import laptop4 from './../../../assets/images/laptop4.jpg'
 
 const SliderComponent = () => {
     return (
 		<div className='slider'>
-			<div className='slide-track'>
-				
+			<div className='slider-track'>
+				{/* the first set */}
+				<div className='slide'>
+					<img src={laptop1} />
+				</div>
+				<div className='slide'>
+					<img src={laptop2} />
+				</div>
+				<div className='slide'>
+					<img src={laptop3} />
+				</div>
+				<div className='slide'>
+					<img src={laptop4} />
+				</div>
+				{/* the second set */}
+				<div className='slide'>
+					<img src={laptop1} />
+				</div>
+				<div className='slide'>
+					<img src={laptop2} />
+				</div>
+				<div className='slide'>
+					<img src={laptop3} />
+				</div>
+				<div className='slide'>
+					<img src={laptop4} />
+				</div>
 			</div>
 		</div>
     )
