eb994df7d43bd2122b0b9762e67c28a696e59774
Fariba <fariba.ghorbani2127@gmail.com>
2022-05-25 19:26:41 +0430

    feat: Add slider component in the home page

--- src/pages/Home/Home.page.js
+++ src/pages/Home/Home.page.js
@@ -5,6 +5,7 @@ import laptop1 from '../../assets/images/gallery-1-2.jpg'
 import laptop2 from '../../assets/images/gallery-8-1.jpg'
 import laptop3 from '../../assets/images/laptop2.jpg'
 import laptop4 from '../../assets/images/laptop4.jpg'
+import SliderComponent from './Components/Slider.component.js'
 
 const HomePage = () => {
 // 	const images = [
@@ -29,6 +30,7 @@ const HomePage = () => {
     return (
         <div>
           	{/* <GallerySlider images={images} /> */}
+			  <SliderComponent />
           
         </div>
     )
