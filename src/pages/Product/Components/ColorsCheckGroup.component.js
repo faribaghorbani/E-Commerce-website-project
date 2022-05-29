1e30744263bc1b88b6e6cc5da7c4cfe4066c875b
Fariba <fariba.ghorbani2127@gmail.com>
2022-05-29 18:09:32 +0430

    feat: Add styles to the color selector component
    - add check mark for the selected color

--- src/pages/Product/Components/ColorsCheckGroup.component.js
+++ src/pages/Product/Components/ColorsCheckGroup.component.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { v4 as uuidv4 } from 'uuid';
 import './style/ColorsCheckGroup.scss'
+import { BsCheck } from 'react-icons/bs'
 
 
 const ColorsCheckGroup = ({colors, selectColor, selectedColor}) => {
@@ -14,15 +15,21 @@ const ColorsCheckGroup = ({colors, selectColor, selectedColor}) => {
 
             {colors?.map((col, index) => {
                 return (
-                    <div 
-                    className='color'
-                    key={uuidv4()}
-                    style={{
-                    backgroundColor: `${col}`,
-                    border: `${index === selectedColor? `4px solid blue`: `2px solid black`}`
-                    }}
-                    onClick={() => handleChangeSelectedColor(index)}
-                    ></div>
+                    <>
+                        <div 
+                        className='color'
+                        key={uuidv4()}
+                        style={{
+                        background: `${col} content-box`,
+                        border: `${index === selectedColor? `4px solid blue`: `2px solid black`}`
+                        }}
+                        onClick={() => handleChangeSelectedColor(index)}
+                        >
+                            <BsCheck className='icon'
+                            style={{display: `${index === selectedColor? `block`:'none'}`}}
+                            />
+                        </div>
+                    </>
                 )
             })}
         
