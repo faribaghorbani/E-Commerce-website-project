b1b7fa8d5b1defc84d71599dc873acb87eb9f3fe
Fariba <fariba.ghorbani2127@gmail.com>
2022-06-01 00:47:28 +0430

    feat: add scss module style for basket table

--- src/pages/Basket/Components/BasketTable.component.js
+++ src/pages/Basket/Components/BasketTable.component.js
@@ -1,12 +1,18 @@
-import React from 'react'
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import './style/BasketTable.scss'
 
-const BasketTable = () => {
+const BasketTable = ({data}) => {
+	
 	const basketProducts = useSelector(state => state.basketProducts)
+
+	useEffect(() => {
+		console.log(basketProducts)
+	}, [])
 	
 	return (
-		<div>
-		
+		<div className='basket-table'>
+			{}
 		</div>
 	)
 }
