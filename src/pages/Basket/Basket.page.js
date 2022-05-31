403b41b5ba1d6f2f23a8c0968f73d10aa1fdc3ec
Fariba <fariba.ghorbani2127@gmail.com>
2022-06-01 00:45:36 +0430

    feat: Add the basis for the basket page
    - fetch the data from server to compare the selected products quantity

--- src/pages/Basket/Basket.page.js
+++ src/pages/Basket/Basket.page.js
@@ -1,12 +1,54 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import LoadingPage from '../Loading/Loading.page';
 import BasketTable from './Components/BasketTable.component';
+import { useSelector, useDispatch } from 'react-redux';
+import { changeStatusBasketProducts } from './../../redux/slices/basketProductsSlice'
 
 const BasketPage = () => {
-	return (
-		<div>
-			<BasketTable />
-		</div>
-	)
+	const dispatch = useDispatch()
+	const [data, setData] = useState([])
+	const [loading, setLoading] = useState(true)
+	const [error, setError] = useState(false)
+	const basketProducts = useSelector(state => state.basketProducts)
+
+	useEffect(() => {
+		axios.get('/products')
+		.then(res => {
+			setData(res.data)
+			Object.entries(basketProducts).forEach(item => {
+				const findSourceInData = res.data.find(product => product.id == item[0])
+				if (findSourceInData == undefined) {
+					dispatch(changeStatusBasketProducts({id : item[0], status: 'deleted'}))
+				} else if (findSourceInData && findSourceInData.quantity < item[1].quantity) {
+					dispatch(changeStatusBasketProducts({id : item[0], status: 'not-enough'}))
+				} else if (findSourceInData && findSourceInData.quantity == 0) {
+					dispatch(changeStatusBasketProducts({id : item[0], status: 'not-existed'}))
+				} else {
+					dispatch(changeStatusBasketProducts({id : item[0], status: 'normal'}))
+				}
+			})
+			setLoading(false)
+		})
+		.catch(err => {
+			setLoading(false)
+			setError(true)
+		})
+	}, [])
+
+
+	if (loading) {
+		return <LoadingPage />
+	} else if (error) {
+		return <div></div>
+	} else {
+		return (
+			<div className='basket-page'>
+				hello
+				<BasketTable/>
+			</div>
+		)
+	}
 }
 
 export default BasketPage;
