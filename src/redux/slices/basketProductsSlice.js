be0eeb5b2277607c7865f4240d995f7dc43a7431
Fariba <fariba.ghorbani2127@gmail.com>
2022-06-01 15:45:12 +0430

    feat: add former quantity for the basket database
    - status: not enough should have an extra field

--- src/redux/slices/basketProductsSlice.js
+++ src/redux/slices/basketProductsSlice.js
@@ -12,6 +12,8 @@ export const basketProductsSlice = createSlice({
         changeNumberBasketProducts: (state, action) => {
             if (action.payload.quantity <= 0) {
                 delete state[action.payload.product.id]
+            } else if (action.payload.formerQuantity !== undefined) {
+                state[action.payload.product.id] = {...state[action.payload.product.id], quantity: action.payload.quantity, formerQuantity: action.payload.formerQuantity}
             } else {
                 state[action.payload.product.id] = {...state[action.payload.product.id], quantity: action.payload.quantity}
             }
