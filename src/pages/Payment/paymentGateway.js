3003c5cc66f3f06172f9252799538b25456b5284
Fariba <fariba.ghorbani2127@gmail.com>
2022-06-03 02:47:55 +0430

    feat: complete redirection from mock payment html

--- src/pages/Payment/paymentGateway.js
+++ src/pages/Payment/paymentGateway.js
@@ -1,14 +1,14 @@
-const MAIN_PATH = 'http://localhost:3000'
+const MAIN_PATH = 'http://localhost:3000/checkout/'
+const succedURL = 'succed'
+const failedURL = 'failed'
 const payButton = document.getElementById('pay')
 const cancelButton = document.getElementById('cancel')
 
 payButton.addEventListener('click', (e) => {
     console.log('pay')
-    window.open(MAIN_PATH)
+    window.open(MAIN_PATH + succedURL)
 })
 cancelButton.addEventListener('click', (e) => {
     console.log('cancel')
-    window.open(MAIN_PATH)
-
-
+    window.open(MAIN_PATH + failedURL)
 })
