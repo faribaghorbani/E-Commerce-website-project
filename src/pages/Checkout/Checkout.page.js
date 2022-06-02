eb8304640553808d98d24bffe51635a0fff5bafd
Fariba <fariba.ghorbani2127@gmail.com>
2022-06-02 18:41:45 +0430

    feat: complete the checkout page
    - complete the validation
    - complete the style

--- src/pages/Checkout/Checkout.page.js
+++ src/pages/Checkout/Checkout.page.js
@@ -1,12 +1,22 @@
 import React from 'react'
+import Container from '@mui/material/Container';
 import CheckoutForm from './Components/CheckoutForm.component';
+import { Grid } from '@mui/material';
+import './Checkout.scss'
 
 const CheckoutPage = () => {
 
 	return (
-		<div className='checkout-page'>
-			<CheckoutForm />
-		</div>
+		<Container maxWidth="xl" className='checkout-page'>
+			<Grid container spacing={2}>
+				<Grid item sm={12}>
+					<CheckoutForm />
+				</Grid>
+				{/* <Grid item md={3} sm={6}>
+					<h1>hello</h1>
+				</Grid> */}
+			</Grid>
+		</Container>
 	)
 }
 
--- src/pages/Checkout/Components/CheckoutForm.component.js
+++ src/pages/Checkout/Components/CheckoutForm.component.js
@@ -9,8 +9,12 @@ import persian from "react-date-object/calendars/persian"
 import persian_fa from "react-date-object/locales/persian_fa"
 import "react-multi-date-picker/styles/colors/red.css"
 import RTL from '../../../components/RTL.component';
-import { TextField, Button } from '@mui/material';
+import { TextField, Button, Paper } from '@mui/material';
+import './style/CheckoutForm.scss'
 
+const phoneRegex = RegExp(
+	/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{5})$/
+);
 
 const validationSchema = yup.object().shape({
 	name: yup.string()
@@ -22,6 +26,9 @@ const validationSchema = yup.object().shape({
 	datepicker: yup.array()
 	.min(2, 'لطفا یک بازه تحویل کامل انتخاب کنید')
 	.required('لطفا بازه تحویل را وارد کنید'),
+	phone: yup.string()
+	.matches(phoneRegex, "شماره موبایل معتبر نیست")
+	.required("لطفا شماره موبایل خود را وارد کنید")
 });
 
 
@@ -33,6 +40,7 @@ const CheckoutForm = () => {
 		  name: '',
 		  lastName: '',
 		  address: '',
+		  phone: '',
 		  datepicker: []
 		},
 		validationSchema: validationSchema,
@@ -47,12 +55,15 @@ const CheckoutForm = () => {
 	}, [dataRange])
 
 	return (
-		<div>
-			<form onSubmit={formik.handleSubmit} style={{ direction: "rtl" }}>
-				<div>
+		<Paper elevation={5} className={'checkout-form-container'}>
+			<div className='banner'>
+				فرم تکمیل خرید
+			</div>
+			<form onSubmit={formik.handleSubmit} className={'checkout-form'}>
+				<div className='name-lastname'>
 					<RTL>
 						<TextField
-							dir='rtl'
+							fullWidth
 							sx={{mb: 3}}
 							label="نام"
 							id="name2"
@@ -66,7 +77,7 @@ const CheckoutForm = () => {
 					</RTL>
 					<RTL>
 						<TextField
-							dir='rtl'
+							fullWidth
 							sx={{mb: 3}}
 							label="نام خانوادگی"
 							id="lastName2"
@@ -79,9 +90,11 @@ const CheckoutForm = () => {
 						/>
 					</RTL>
 				</div>
+
 				<RTL>
 					<TextField
 						fullWidth
+						sx={{mb: 3}}
 						id="address2"
 						label="آدرس"
 						name="address"
@@ -93,35 +106,59 @@ const CheckoutForm = () => {
 						helperText={formik.touched.address && formik.errors.address}
 					/>
 				</RTL>
-				<DatePicker
-					onChange={(dateobject) => {
-						formik.setFieldValue("datepicker", dateobject);
-						setDateRange(dateobject)
-					}}
-					value={formik.values.datepicker}
-					className="red"
-					animations={[
-						opacity(),
-						transition({
-						  from: 40,
-						  transition: "all 400ms cubic-bezier(0.335, 0.010, 0.030, 1.360)",
-						}),
-					]}
-					minDate={new DateObject({ calendar: persian })}
-					range 
-					calendar={persian}
-					locale={persian_fa}
-					calendarPosition="bottom-right"
-					name={"datapicker"}
-				/>
-				
-				<label>{formik.touched.datepicker && formik.errors.datepicker}</label>
-
-				<Button color="primary" type="submit">
-					ثبت
-				</Button>
+				<div className='phone-date'>
+					<RTL>
+						<TextField
+							dir='rtl'
+							sx={{mb: 3}}
+							label="شماره موبایل"
+							id="phone2"
+							name="phone"
+							type="text"
+							value={formik.values.phone}
+							onChange={formik.handleChange}
+							error={formik.touched.phone && Boolean(formik.errors.phone)}
+							helperText={formik.touched.phone && formik.errors.phone}
+						/>
+					</RTL>
+					<div className='date-box'>
+						<DatePicker
+							onChange={(dateobject) => {
+								formik.setFieldValue("datepicker", dateobject);
+								setDateRange(dateobject)
+							}}
+							value={formik.values.datepicker}
+							className="red"
+							containerStyle={{
+								width: "100%"
+							}}
+							animations={[
+								opacity(),
+								transition({
+								from: 40,
+								transition: "all 400ms cubic-bezier(0.335, 0.010, 0.030, 1.360)",
+								}),
+							]}
+							minDate={new DateObject({ calendar: persian })}
+							range 
+							calendar={persian}
+							locale={persian_fa}
+							calendarPosition="bottom-right"
+							name={"datapicker"}
+						/>
+						<span className='date-error'>{formik.touched.datepicker && formik.errors.datepicker}</span>
+					</div>
+				</div>
+				<div className='actions'>
+					<Button variant="contained" color="success" type="submit">
+						درگاه پرداخت
+					</Button>
+					<Button variant="contained" className='go-basket-button'>
+						سبد خرید
+					</Button>
+				</div>
 			</form>
-		</div>
+		</Paper>
 	)
 }
 
