c0a31f11dae9d68a58c5b3bc00e4f986f8b53e2a
Fariba <fariba.ghorbani2127@gmail.com>
2022-06-03 10:31:46 +0430

    feat: add counter for basket icon in header

--- src/components/Header.component.js
+++ src/components/Header.component.js
@@ -1,17 +1,56 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import logo from './../assets/images/logo.png'
 import './style/Header.scss'
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { useNavigate } from 'react-router-dom';
+import Badge from '@mui/material/Badge';
+import { styled } from '@mui/material/styles';
+import IconButton from '@mui/material/IconButton';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { useSelector } from 'react-redux';
+
+
+const StyledBadge = styled(Badge)(({ theme }) => ({
+	'& .MuiBadge-badge': {
+	  right: -3,
+	  top: 13,
+	  border: `2px solid ${theme.palette.background.paper}`,
+	  padding: '0 4px',
+	},
+}));
+
 
 const Header = () => {
-  const navigate = useNavigate()
-  return (
+	const basketProducts = useSelector(state => state.basketProducts)
+	const navigate = useNavigate()
+	const [basketBadge, setBasketBadge] = useState(0)
+
+	useEffect(() => {
+		let sum = 0
+        Object.entries(basketProducts).forEach(item => {
+            if (item[1].status == 'normal' ||  item[1].status == 'not-enough') {
+                sum += (item[1].quantity)
+            }
+        })
+        setBasketBadge(sum)
+	}, [basketProducts])
+
+	return (
 		<div className='regular-layout-header'>
-			<div className='actions'>
+			<div className='actions' style={{display: 'flex', justifyContent: 'center', alignItems: 'stretch'}}>
 				<Button className='admin-entrance' variant="contained" onClick={() => {navigate('/panel')}}>ورود ادمین</Button>
-				<Button className='basket-entrance' variant="contained" onClick={() => {navigate('/basket')}}>سبد خرید</Button>
+				
+				{basketBadge?
+				(<IconButton aria-label="cart" style={{color: 'white'}} onClick={() => {navigate('/basket')}}>
+					<StyledBadge badgeContent={basketBadge} color="secondary">
+						<ShoppingCartIcon />
+					</StyledBadge>
+				</IconButton>) : 
+				(<IconButton aria-label="cart" style={{color: 'white'}} onClick={() => {navigate('/basket')}}>
+					<ShoppingCartIcon />
+				</IconButton>) 
+				}
 			</div>
 
 			<ul className='navbar-items'>
