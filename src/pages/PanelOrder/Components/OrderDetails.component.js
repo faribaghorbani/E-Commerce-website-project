806b7c1df2947adee947f9252078ff048e4c709d
Fariba <fariba.ghorbani2127@gmail.com>
2022-06-03 09:14:55 +0430

    feat: axios patch for change delivery status

--- src/pages/PanelOrder/Components/OrderDetails.component.js
+++ src/pages/PanelOrder/Components/OrderDetails.component.js
@@ -4,8 +4,16 @@ import persian_fa from "react-date-object/locales/persian_fa"
 import DateObject from "react-date-object";
 import { Button, Divider } from '@mui/material';
 import OrderDetailsTable from './OrderDetailsTable.component';
+import './style/OrderDetails.scss'
+import axios from 'axios';
+import { getData } from '../../../services/http.service';
+import { useDispatch } from 'react-redux';
+import { setOrders } from '../../../redux/slices/allOrdersSlice';
+import { useNavigate } from 'react-router-dom';
 
 const OrderDetails = ({orderInModal, closeModal}) => {
+	const dispatch = useDispatch()
+	const navigate = useNavigate()
 	const [deliveredAt, setDeliveredAt] = useState(null)
 
 	const deliveryRangeBegin = new DateObject({calendar: persian, locale: persian_fa },JSON.parse(orderInModal.deliveryRange[0])).format()
@@ -20,11 +28,23 @@ const OrderDetails = ({orderInModal, closeModal}) => {
 		else {
 			setDeliveredAt(new DateObject({calendar: persian, locale: persian_fa },JSON.parse(orderInModal.deliveredAt)).format())
 		}
-	}, [])
+	}, [orderInModal])
 
 
 	const handleDeliverActions = (id) => {
-		
+		axios.patch(`/orders/${id}`, {
+			orderStatusId: 6,
+			deliveredAt: Date.now()
+		})
+		.then(res => {
+			getData('/orders',
+			(data) => {
+				dispatch(setOrders(data))
+			},
+			() => navigate("/login", {replace: true})
+			)
+			closeModal()
+		})
 	}
 
 
@@ -42,9 +62,9 @@ const OrderDetails = ({orderInModal, closeModal}) => {
 			<Divider />
 			<OrderDetailsTable orderItems={orderInModal.orderItems} />
 			<Divider />
-			<div className='delivery-actions'>
+			<div className='delivery-actions' style={{display: 'flex', justifyContent: 'center', margin: '20px'}}>
 				{orderInModal.orderStatusId == 6? (<h5>تاریخ تحویل: {deliveredAt}</h5>)
-					: orderInModal.orderStatusId == 3 ? (<Button variant='contained' color="success">لغو شد</Button>)
+					: orderInModal.orderStatusId == 3 ? (<Button variant='contained' color="success" disabled>لغو شد</Button>)
 					: orderInModal.orderStatusId == 1 ? (<Button variant='contained' color="success" onClick={() => handleDeliverActions(orderInModal.id)}>تحویل شد</Button>)
 					: null
 				}
