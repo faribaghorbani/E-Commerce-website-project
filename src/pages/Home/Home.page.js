ed36c6af9ee1223ae9a92a1565086ab78d16f771
Fariba <fariba.ghorbani2127@gmail.com>
2022-06-09 15:05:34 +0430

    feat: add some animation (3d model and 3d slider)

--- src/pages/Home/Home.page.js
+++ src/pages/Home/Home.page.js
@@ -1,49 +1,80 @@
-import React, { useEffect, useState } from 'react'
+import React, { Suspense, useEffect, useState } from 'react'
 import './Home.scss'
 import SliderComponent from './Components/Slider.component.js'
 import anime from 'animejs'
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import GallerySlider from './Components/GallerySlider.component';
+import { Grid, Box} from '@mui/material';
+const Spline = React.lazy(() => import('@splinetool/react-spline'));
+
 
 const HomePage = () => {
 	const navigate = useNavigate()
 	const [laptops, setLaptops] = useState([])
 	const [phones, setPhones] = useState([])
 
-	// const animation = () => {
-	// 	anime({
-	// 		targets: 'svg path',
-	// 		d: [
-	// 		  { value: 'M0 223L21.5 218.5C43 214 86 205 128.8 203.8C171.7 202.7 214.3 209.3 257.2 217.5C300 225.7 343 235.3 385.8 240.7C428.7 246 471.3 247 514.2 233.3C557 219.7 600 191.3 642.8 186.7C685.7 182 728.3 201 771.2 209.7C814 218.3 857 216.7 878.5 215.8L900 215L900 0L878.5 0C857 0 814 0 771.2 0C728.3 0 685.7 0 642.8 0C600 0 557 0 514.2 0C471.3 0 428.7 0 385.8 0C343 0 300 0 257.2 0C214.3 0 171.7 0 128.8 0C86 0 43 0 21.5 0L0 0Z' },
-	// 		  { value: 'M0 143L21.5 138.5C43 134 86 125 128.8 119.7C171.7 114.3 214.3 112.7 257.2 123C300 133.3 343 155.7 385.8 154.7C428.7 153.7 471.3 129.3 514.2 141.3C557 153.3 600 201.7 642.8 229.5C685.7 257.3 728.3 264.7 771.2 246.5C814 228.3 857 184.7 878.5 162.8L900 141L900 0L878.5 0C857 0 814 0 771.2 0C728.3 0 685.7 0 642.8 0C600 0 557 0 514.2 0C471.3 0 428.7 0 385.8 0C343 0 300 0 257.2 0C214.3 0 171.7 0 128.8 0C86 0 43 0 21.5 0L0 0Z' }
-	// 		],
-	// 		easing: 'linear',
-	// 		duration: 2000,
-	// 		direction: 'alternate',
-	// 		delay: 0,
-	// 		loop: true
-	// 	  });
-	// }
+	const animationLandingSVGDivider = () => {
+		anime({
+			targets: '#svg1 path',
+			d: [
+			  { value: 'M0,96L60,122.7C120,149,240,203,360,197.3C480,192,600,128,720,128C840,128,960,192,1080,192C1200,192,1320,128,1380,96L1440,64L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z' },
+			  { value: 'M0,64L60,101.3C120,139,240,213,360,245.3C480,277,600,267,720,229.3C840,192,960,128,1080,112C1200,96,1320,128,1380,144L1440,160L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z' },
+			  { value: 'M0,192L60,165.3C120,139,240,85,360,90.7C480,96,600,160,720,186.7C840,213,960,203,1080,208C1200,213,1320,235,1380,245.3L1440,256L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z' }
+			],
+			easing: 'linear',
+			duration: 5000,
+			direction: 'alternate',
+			delay: 0,
+			loop: true
+		  });
+	}
 
 	useEffect(() => {
 		const requests = [
 			axios.get('/products?category.main=laptop&_page=1&_limit=6'),
 			axios.get('/products?category.main=phone')
 		]
-		Promise.all(requests)
+		animationLandingSVGDivider()
 
+		Promise.all(requests)
 		.then(([laptops, phones]) => {
-			console.log(laptops.data)
 			setLaptops(laptops.data)
-			console.log(phones.data)
 			setPhones(phones.data)
 		})
-		
 	}, [])
 
 
     return (
-        <div>
+        <div className='home-page'>
+			<div className='landing-view'>
+				<svg id='svg1' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 300">
+					<path fill="#FFFFFF" fillOpacity="1" d="M0,32L60,69.3C120,107,240,181,360,192C480,203,600,149,720,106.7C840,64,960,32,1080,53.3C1200,75,1320,149,1380,186.7L1440,224L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path>
+				</svg>
+
+				<Grid container spacing={2}>
+					<Grid item sm={12}  md={12} lg={7} display={{sm: 'block', xs: 'none'}}>
+						<Box className='spline-container'>
+							<Suspense fallback={<div style={{minWidth: '300px'}}></div>}>
+								<Spline
+								className='spline-macbook' 
+								canvasStyle={{width: "100%"}} 
+								style={{width: "100%", height: '100%', overflow: 'hidden'}}
+								scene="https://prod.spline.design/3MhcQsqMIcXvjSVt/scene.splinecode" />
+							</Suspense>
+						</Box>
+					</Grid>
+					<Grid item md={12} lg={5}>
+						<Box dir='rtl' sx={{color: "white", display: "flex", justifyContent: "center", alignItems: 'center'}}>
+							به فروشگاه سیلیکان مال خوش آمدید
+						</Box>
+					</Grid>
+				</Grid>
+			</div>
+
+			<GallerySlider />
+
+
 			<div className='main-category laptop'>
 				<h2 className='title' onClick={() => navigate('/products/laptop?page=1')}>لپ تاپ</h2>
             	<SliderComponent cards={laptops} dir={'right'}/>
@@ -52,6 +83,7 @@ const HomePage = () => {
 				<h2 className='title' onClick={() => navigate('/products/phone?page=1')}>موبایل</h2>
             	<SliderComponent cards={phones} dir={'left'}/> 
 			</div>
+
         </div>
     )
 }
