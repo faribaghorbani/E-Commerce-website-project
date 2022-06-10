7d6adcf7d2c31ef40bdd00ce88b95cc7c7e6d45a
Fariba <fariba.ghorbani2127@gmail.com>
2022-06-10 14:42:08 +0430

    feat: add serachbar fetching to the header

--- src/components/Searchbox.component.js
+++ src/components/Searchbox.component.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { styled, alpha } from '@mui/material/styles';
 import InputBase from '@mui/material/InputBase';
 import SearchIcon from '@mui/icons-material/Search';
+import _ from 'lodash';
+import { useCallback } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { getData } from '../services/http.service';
+import { Menu } from '@mui/material';
+import { MenuItem } from '@mui/material';
+import { Box } from '@mui/system';
 
 const Search = styled('div')(({ theme }) => ({
 	position: 'relative',
@@ -49,18 +56,95 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
 	},
 }));
 
+const mobileMenuId = 'primary-search-account-menu-mobile';
 
 const Searchbox = () => {
+	const navigate = useNavigate()
+	const [value, setValue] = useState("")
+	const [data, setData] = useState([])
+	const [anchorEl, setAnchorEl] = useState(null);
+	const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = useState(null);
+
+	const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+	const handleProfileMenuOpen = (event) => {
+		setAnchorEl(event.currentTarget);
+	  };
+
+	const handleMobileMenuClose = () => {
+		setMobileMoreAnchorEl(null);
+	  };
+	
+	  const handleMenuClose = () => {
+		setAnchorEl(null);
+		handleMobileMenuClose();
+	  };
+	
+	  const handleMobileMenuOpen = (event) => {
+		setMobileMoreAnchorEl(event.currentTarget);
+	  };
+	
+
+	const optimisedSearching = useCallback(_.throttle((e) => {
+		getData(`/products?name_like=${e.target.value}`,
+		// getData(`/products`,
+		(data) => {
+			setData(data)
+			console.log(data)
+		},
+		() => navigate("/login", {replace: true})
+	)
+	}, 1000), [navigate])
+
+	const handleChange = (e) => {
+		setValue(e.target.value)
+		optimisedSearching(e)
+	}
+
 	return (
-		<Search dir='rtl'>
-			<SearchIconWrapper>
-				<SearchIcon />
-			</SearchIconWrapper>
-			<StyledInputBase
-			placeholder="جست و جو ..."
-			inputProps={{ 'aria-label': 'search' }}
-			/>
-		</Search>
+		<Box sx={{position: 'relative'}}>
+			<Search dir='rtl'>
+				<SearchIconWrapper>
+					<SearchIcon />
+				</SearchIconWrapper>
+				<StyledInputBase
+				placeholder="جست و جو ..."
+				inputProps={{ 'aria-label': 'search' }}
+				value={value}
+				onChange={(e) => {
+					handleChange(e)
+					handleMobileMenuOpen(e)
+
+				}}
+				/>
+			</Search>
+			<Menu
+			sx={{position: 'absolute', top: '50px', height: "400px", overflowY: "scroll"}}
+			anchorEl={mobileMoreAnchorEl}
+			anchorOrigin={{
+				vertical: 'top',
+				horizontal: 'right',
+			}}
+			id={mobileMenuId}
+			keepMounted
+			transformOrigin={{
+				vertical: 'top',
+				horizontal: 'right',
+			}}
+			open={isMobileMenuOpen}
+			onClose={handleMobileMenuClose}
+			>
+				{data.map((item) => {
+					return (
+					<MenuItem dir='rtl'>
+						<Link to={`/products/${item.category.main}/${item.category.second}/${item.id}`} style={{textDecoration: 'none', color: 'inherit'}}>
+							{item.name}
+						</Link>
+					</MenuItem>
+					)
+				})}
+			</Menu>
+		</Box>
 	)
 }
 
