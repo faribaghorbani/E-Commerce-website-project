c44d19ca8ab5b1f7d88e3b39063de1ad93b9cb30
Fariba <fariba.ghorbani2127@gmail.com>
2022-06-10 11:13:15 +0430

    feat: slider pause on hover event

--- src/pages/Home/Components/Slider.component.js
+++ src/pages/Home/Components/Slider.component.js
@@ -2,11 +2,13 @@ import React from 'react'
 import './style/SliderLeft.scss'
 import ProductCard from '../../../components/ProductCard.component'
 import { v4 as uuidv4 } from 'uuid';
+import { Box } from '@mui/system';
 
 
 const SliderComponent = ({cards, dir}) => {
 
     return (
+		<Box className='slider-container'>
 		<div className='slider'>
 			<div className='slider-track'>
 			
@@ -27,6 +29,7 @@ const SliderComponent = ({cards, dir}) => {
 
 			</div>
 		</div>
+		</Box>
     )
 }
 
